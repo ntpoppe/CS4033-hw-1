fd1666a195475a99919869a276ee9c6138f2f3f8
Rashi455 <rashiloni@gmail.com>
2025-09-21 13:39:36 -0400

    added one heruistic and another test case

--- heuristics.py
+++ heuristics.py
@@ -1,3 +1,4 @@
+import math
 # heuristic from textbook: straight line distances to Bucharest
 hSLD_bucharest = {
     "Arad": 366, "Bucharest": 0, "Craiova": 160, "Drobeta": 242,
@@ -7,3 +8,27 @@ hSLD_bucharest = {
     "Sibiu": 253, "Timisoara": 329, "Urziceni": 80,
     "Vaslui": 199, "Zerind": 374
 }
+
+def heuristic1(goal, romania_adj_list):
+    cities_list = romania_adj_list.keys()
+    h ={}
+    for city in cities_list:
+        h[city] = math.inf
+    h[goal] = 0
+
+    distance_changed = True
+    while distance_changed == True:
+        distance_changed = False
+        for city in cities_list:
+            neighbors = romania_adj_list[city]
+            for neighbor in neighbors:
+                distance = neighbors[neighbor]
+            
+            # if you find a shorter path
+            if h[city] > h[neighbor] + distance:
+                h[city] = h[neighbor] + distance
+                distance_changed = True
+    return h
+
+
+
