39a62038b6e7bd9acb528c721fdb80d13e602740
nailahsall0 <83038251+nailahsall0@users.noreply.github.com>
2025-09-21 18:34:20 -0400

    Added heuristcs 2 function

--- textbook_objects/heuristics.py
+++ textbook_objects/heuristics.py
@@ -40,6 +40,22 @@ def heuristic1(goal, romania_adj_list):
                 h[city] = h[neighbor] + distance
                 distance_changed = True
     return h
+def heuristic2(goal, romania_adj_list):
+    cities_list = romania_adj_list.keys()
+    h = {}
+
+    for city in cities_list:
+        if city == goal:
+            h[city] = 0 #the heuristic for the goal is always 0
+        else:
+            neighbors = romania_adj_list[city]
+            estimates = []
+            for neighbor, dist in neighbors.items(): 
+                neighbor_est = 0 if neighbor == goal else hSLD_bucharest.get(neighbor, 999) #if neighbor is the goal use edge distanc, else use SLD
+                estimates.append(dist + neighbor_est)
+                #choose the smallest estimate
+            h[city] = min(estimates) if estimates else 9999 #if there are no neighbors use fallback num
+    return h
 
 
 
